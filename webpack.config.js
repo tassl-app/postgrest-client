d4c8dca6eab6db54ed1740127752adffe964199f
Rob Capell <rob@robcapell.com>
2017-08-09 13:44:39 -0400

    Importing lodash compact differently, old way was causing errors.

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,36 @@
 'use strict'
 
 const webpack = require('webpack')
-const compact = require('lodash/array/compact')
+const _ = require('lodash')
+const compact = _.compact
 
 const optimize = webpack.optimize
 
 const minify = process.env.MINIFY === 'true'
 
 const banner = [
-  'PostgREST Client',
-  '(c) 2016 Caleb Meredith',
-  'Released under the MIT License.'
+    'PostgREST Client',
+    '(c) 2016 Caleb Meredith',
+    'Released under the MIT License.'
 ].join('\n')
 
 module.exports = {
-  entry: './index.js',
-  output: {
-    path: './dist',
-    filename: minify ? 'postgrest-client.min.js' : 'postgrest-client.js',
-    library: 'PostgREST',
-    libraryTarget: 'var'
-  },
-  target: 'web',
-  bail: true,
-  plugins: compact([
-    new webpack.BannerPlugin(banner),
-    new optimize.DedupePlugin(),
-    minify ?
-    new optimize.UglifyJsPlugin({
-      mangle: true,
-      comments: /PostgREST Client/
-    }) : null
-  ])
+    entry: './index.js',
+    output: {
+        path: './dist',
+        filename: minify ? 'postgrest-client.min.js' : 'postgrest-client.js',
+        library: 'PostgREST',
+        libraryTarget: 'var'
+    },
+    target: 'web',
+    bail: true,
+    plugins: compact([
+        new webpack.BannerPlugin(banner),
+        new optimize.DedupePlugin(),
+        minify ?
+        new optimize.UglifyJsPlugin({
+            mangle: true,
+            comments: /PostgREST Client/
+        }) : null
+    ])
 }
